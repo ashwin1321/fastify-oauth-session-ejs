768886ef7322f46cc1bf8a6578e88755c6897030
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-05-25 13:08:18 +0545

    feat: Controller for todo app

---
+++ controllers/todoControllers.js
@@ -0,0 +1,61 @@
+export default {
+  getTodos: async (request, reply) => {
+    try {
+      const todos = await request.server.Todo.findAll(); // Retrieve all todos from the Todos table
+
+      if (todos.length === 0) {
+        return reply.code(404).send("No todos found");
+      }
+
+      reply.code(200).send(todos);
+    } catch (error) {
+      console.error("Error retrieving todos", error);
+      reply.code(500).send("Internal Server Error");
+    }
+  },
+
+  createTodo: async (request, reply) => {
+    try {
+      const { title, completed } = request.body;
+      const newTodo = await request.server.Todo.create({ title, completed }); // Create a new todo in the Todos table
+      reply.code(201).send(newTodo);
+    } catch (error) {
+      console.error("Error creating a todo", error);
+      reply.code(500).send("Internal Server Error");
+    }
+  },
+
+  updateTodo: async (request, reply) => {
+    try {
+      const { id } = request.params;
+      const { title, completed } = request.body;
+      const [updatedRowsCount] = await request.server.Todo.update(
+        { title, completed }, // Update the title and completed fields
+        { where: { id } } // Filter by the todo ID
+      );
+      if (updatedRowsCount === 0) {
+        return reply.code(404).send("Todo not found");
+      }
+      reply.code(200).send("Todo updated successfully");
+    } catch (error) {
+      console.error("Error updating a todo", error);
+      reply.code(500).send("Internal Server Error");
+    }
+  },
+
+  deleteTodo: async (request, reply) => {
+    try {
+      const { id } = request.params;
+      const deletedRowsCount = await request.server.Todo.destroy({
+        where: { id }, // Filter by the todo ID
+      });
+      if (deletedRowsCount === 0) {
+        return reply.code(404).send("Todo not found");
+      }
+      reply.code(200).send("Todo deleted successfully");
+    } catch (error) {
+      console.error("Error deleting a todo", error);
+      reply.code(500).send("Internal Server Error");
+    }
+  },
+};

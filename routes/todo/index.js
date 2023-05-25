0417b5cf4632491e419ad2a7ab51cf2616b63740
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-05-25 13:11:34 +0545

    feat: Add schemas to route

--- routes/todo/index.js
+++ routes/todo/index.js
@@ -1,8 +1,29 @@
 import todoControllers from "../../controllers/todoControllers.js";
+import {
+  getTodosOpts,
+  createTodoOpts,
+  updateTodoOpts,
+  deleteTodoOpts,
+} from "../../Schemas/todoSchema.js";
 
 export default async function (fastify, opts) {
-  fastify.get("/", todoControllers.getTodos);
-  fastify.post("/", todoControllers.createTodo);
-  fastify.put("/:id", todoControllers.updateTodo);
-  fastify.delete("/:id", todoControllers.deleteTodo);
+  fastify.get("/", { schema: getTodosOpts.schema }, todoControllers.getTodos);
+
+  fastify.post(
+    "/",
+    { schema: createTodoOpts.schema },
+    todoControllers.createTodo
+  );
+
+  fastify.put(
+    "/:id",
+    { schema: updateTodoOpts.schema },
+    todoControllers.updateTodo
+  );
+
+  fastify.delete(
+    "/:id",
+    { schema: deleteTodoOpts.schema },
+    todoControllers.deleteTodo
+  );
 }

e1e53bedb7f72cdb4219fce2afa07c4e2af590a3
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-05-29 11:10:58 +0545

    feat: add foreign key to todo table

--- models/todoModel.js
+++ models/todoModel.js
@@ -4,6 +4,24 @@ import fp from "fastify-plugin";
 export default fp(async (fastify, opts) => {
   const sequelize = fastify.sequelize;
 
+  const Users = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
   const Todo = sequelize.define("TodosequelizePostges", {
     id: {
       type: DataTypes.INTEGER,
@@ -20,13 +38,27 @@ export default fp(async (fastify, opts) => {
       allowNull: false,
       defaultValue: false,
     },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Users,
+        key: "id",
+      },
+    },
   });
+
+  // Define the association
+  Todo.belongsTo(Users, { foreignKey: "userId" });
+  Users.hasMany(Todo, { foreignKey: "userId" });
+
   try {
-    await Todo.sync();
-    console.log("Todos table created successfully");
+    await sequelize.sync({ force: true });
+    console.log("User and Todo tables created successfully");
   } catch (error) {
-    console.error("Unable to create todos table:", error);
+    console.error("Unable to create tables:", error);
   }
 
+  fastify.decorate("Users", Users);
   fastify.decorate("Todo", Todo);
 });

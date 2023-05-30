63b379e0c457efd85a7fae415725ab09c16f7e89
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-05-30 14:33:40 +0545

    feat: register oauthplugin

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import oauthPlugin from "@fastify/oauth2";
 import path from "path";
 import AutoLoad from "@fastify/autoload";
 import { fileURLToPath } from "url";
@@ -12,6 +13,31 @@ export const options = {};
 export default async function (fastify, opts) {
   // Place here your custom code!
 
+  fastify.register(oauthPlugin, {
+    name: "googleOAuth2",
+    scope: ["profile", "email"],
+    credentials: {
+      client: {
+        id: process.env.CLIENT_ID,
+        secret: process.env.CLIENT_SECRET,
+      },
+      auth: oauthPlugin.GOOGLE_CONFIGURATION,
+    },
+    startRedirectPath: "/login/google",
+    callbackUri: "http://localhost:3000/login/google/callback",
+  });
+
+  fastify.get("/login/google/callback", async function (request, reply) {
+    const { token } =
+      await this.googleOAuth2.getAccessTokenFromAuthorizationCodeFlow(request);
+
+    console.log(token);
+
+    // if later you need to refresh the token you can use
+    // const { token: newToken } = await this.getNewAccessTokenUsingRefreshToken(token.refresh_token)
+
+    reply.send({ token });
+  });
   // Do not touch the following lines
 
   // This loads all plugins defined in plugins

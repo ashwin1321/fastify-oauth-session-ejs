af7847471ff38ca4cbdc0f9e8d94289318904d52
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-05-31 13:32:39 +0545

    feat: Store in session

--- models/sessionModel.js
+++ models/sessionModel.js
@@ -3,27 +3,16 @@ import fp from "fastify-plugin";
 
 export default fp(async (fastify, opts) => {
   const { sequelize } = fastify;
-  const sessionModel = sequelize.define(
-    "session",
-    {
-      sid: {
-        type: DataTypes.STRING,
-        primaryKey: true,
-        allowNull: false,
-      },
-      sess: {
-        type: DataTypes.JSONB,
-        allowNull: false,
-      },
-      expire: DataTypes.DATE,
-      //   data: DataTypes.STRING(5000),
+  const sessionModel = sequelize.define("sessions", {
+    sid: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false,
     },
-    {
-      tableName: "session",
-      timestamps: false,
-      // force: true,
-    }
-  );
+
+    expires: DataTypes.DATE,
+    data: DataTypes.TEXT,
+  });
   try {
     await sessionModel.sync();
   } catch (err) {
--- plugins/session.js
+++ plugins/session.js
@@ -1,9 +1,12 @@
 import fp from "fastify-plugin";
 import fastifyCookie from "@fastify/cookie";
 import fastifySession from "@fastify/session";
+import connectSessionSequlize from "connect-session-sequelize";
 
 export default fp(async (fastify, opts) => {
   fastify.register(fastifyCookie);
+  var SequelizeStore = connectSessionSequlize(fastifySession.Store);
+
   fastify.register(fastifySession, {
     secret: "my-super-secret-key-with-a-length-of-32-or-more-characters",
     cookie: {
@@ -11,6 +14,10 @@ export default fp(async (fastify, opts) => {
       maxAge: 7 * 24 * 60 * 60 * 1000,
     },
     saveUninitialized: false,
+    store: new SequelizeStore({
+      db: fastify.sequelize,
+      tableName: "sessions",
+    }),
   });
 
   fastify.decorate("authenticate", async (request, reply) => {

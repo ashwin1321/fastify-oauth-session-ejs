30c3e9820e1e1b2fa81f4dacb0817bc6e2b3d82c
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-06-01 12:10:51 +0545

    feat: Get repo informations

---
+++ routes/getGitUserInfo.js
@@ -0,0 +1,41 @@
+import fp from "fastify-plugin";
+import axios from "axios";
+
+export default fp(async function (fastify, opts) {
+  fastify.get(
+    "/getuserdata",
+    { preHandler: fastify.authenticate },
+    async (request, reply) => {
+      //   reply.send(request.users);
+      //   return;
+      const { id, url, token, user } = request.users;
+
+      // Set the authorization header with the token
+      const headers = {
+        Authorization: `Bearer ${token}`,
+      };
+
+      try {
+        // Fetch the repositories using the GitHub REST API
+        const response = await axios.get(
+          `https://api.github.com/users/${user.login}/repos`,
+          {
+            headers,
+          }
+        );
+
+        // Extract the repositories from the response
+        const repositories = response.data.map((repo) => ({
+          name: repo.name,
+          url: repo.html_url,
+          description: repo.description,
+        }));
+
+        reply.send(repositories);
+      } catch (error) {
+        console.error("Error fetching repositories:", error);
+        reply.status(500).send("Error fetching repositories");
+      }
+    }
+  );
+});

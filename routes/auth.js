f816ece70e11e6ff903c74d32c6d283d4b25df89
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-06-06 10:51:29 +0545

    feat: Add prehandler to route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,18 @@
 import fastify from "fastify";
 
 export default async function (fastify, opts) {
-  fastify.get("/user/login", async function (request, reply) {
-    return await reply.view("templates/login.ejs", { message: "" });
-  });
-  fastify.get("/user/register", async function (request, reply) {
-    return reply.view("templates/register.ejs", { message: "" });
-  });
+  fastify.get(
+    "/user/login",
+    { preHandler: fastify.isLogged },
+    async function (request, reply) {
+      return await reply.view("templates/login.ejs", { message: "" });
+    }
+  );
+  fastify.get(
+    "/user/register",
+    { preHandler: fastify.isLogged },
+    async function (request, reply) {
+      return reply.view("templates/register.ejs", { message: "" });
+    }
+  );
 }

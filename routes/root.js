7810f52553d03fcc2adcc73a850b00dbb1ee803a
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-06-06 11:03:43 +0545

    feat: Add prevalidation to route

--- routes/root.js
+++ routes/root.js
@@ -1,5 +1,9 @@
 export default async function (fastify, opts) {
-  fastify.get("/", async function (request, reply) {
-    return await reply.view("templates/index.ejs");
-  });
+  fastify.get(
+    "/",
+    { preHandler: fastify.isLogged },
+    async function (request, reply) {
+      return await reply.view("templates/index.ejs");
+    }
+  );
 }

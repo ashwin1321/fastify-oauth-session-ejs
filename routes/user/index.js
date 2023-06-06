9304ca0dcfa0ed56c77ce3a187be42818110a6c5
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-06-06 15:04:45 +0545

    feat: Register route for otp

--- routes/user/index.js
+++ routes/user/index.js
@@ -4,4 +4,5 @@ export default async function (fastify, opts) {
   fastify.post("/register", UserController.registerUser);
   fastify.post("/login", UserController.loginUser);
   fastify.post("/logout", UserController.logoutUser);
+  fastify.post("/otp", UserController.verifyOtp);
 }

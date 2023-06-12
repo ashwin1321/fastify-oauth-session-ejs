61932b3d3170038a2d3ef54c9775131dba0f7f69
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-06-12 10:42:14 +0545

    feat: Send welcome mail

--- plugins/sendMail.js
+++ plugins/sendMail.js
@@ -8,7 +8,7 @@ import fs from "fs";
 dotenv.config();
 
 export default fp(async (fastify, opts) => {
-  fastify.decorate("sendMail", async (to, otp) => {
+  fastify.decorate("sendMailOtp", async (to, otp) => {
     try {
       let transporter = nodemailer.createTransport({
         service: "Outlook365",
@@ -29,6 +29,38 @@ export default fp(async (fastify, opts) => {
         from: process.env.EMAIL,
         to: to,
         subject: "OTP Verification Code from Todo App",
+        html: renderedTemplate,
+      });
+
+      console.log("Message sent: %s", send.messageId);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  fastify.decorate("sendMailWelcome", async (to) => {
+    try {
+      let transporter = nodemailer.createTransport({
+        service: "Outlook365",
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.PASSWORD,
+        },
+      });
+
+      const name = to.split("@")[0];
+
+      const __fileName = fileURLToPath(import.meta.url);
+      // const templatePath = path.join(__dirname, "../templates/otpFormat.ejs");
+      const __dirname = path.dirname(__fileName);
+      const templatePath = path.join(__dirname, "../templates/welcome.ejs");
+      const template = fs.readFileSync(templatePath, "utf8");
+      const renderedTemplate = ejs.render(template, { name });
+
+      let send = await transporter.sendMail({
+        from: process.env.EMAIL,
+        to: to,
+        subject: `Welcome to Todo App, ${name}!`,
         html: renderedTemplate,
       });
 
